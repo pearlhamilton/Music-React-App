fae5ee162f369823b530218f143605c34a791870
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-12 12:15:14 +0100

    add Artist component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Artist from './artist'
+
+const App = () => {
+return(
+<> 
+    <Artist />
+    <songs />
+    
+
+ </>
+
+)
+}
+
+export default App
+
---
+++ src/artist.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Artist = () => { 
+    return (
+<>
+    <h2>Sofiane Pamart</h2>
+    <p class="musictype">Classical and Hiphop</p>
+    <p> Sofiane Pamart is a French pianist, based in Paris and originally from Hellemmes in the suburbs of Lille. He is known to the French public for breaking the elitist codes of classical piano. A gold medallist at the Conservatoire de Lille, he took a different track to the typical classical music path.</p>
+
+
+
+</>
+    )
+}
+
+export default Artist
