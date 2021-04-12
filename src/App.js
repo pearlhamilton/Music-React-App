b367ef3afd61c2181b7d160b869d16f232e25e77
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-12 12:26:56 +0100

    add songs component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Artist from './artist'
+import Songs from './songs'
 
 const App = () => {
 return(
 <> 
     <Artist />
-    <songs />
+    <Songs />
     
 
  </>
---
+++ src/songs.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Songs = () => {
+    return ( 
+        <table>
+            <tr>
+                <th>Album Name</th>
+                <th>Release Date</th>
+                <th>Cover Art</th>   
+            </tr>
+            <tr>
+                <td>PLANET</td>
+                <td>2020</td>
+                <td><img src="https://images-na.ssl-images-amazon.com/images/I/71fM5jniZVL._AC_SL1500_.jpg"></img></td>
+            </tr>
+            <tr>
+                <td>Pleine Lune 2</td>
+                <td>2019</td>
+                <td><img src="https://img.discogs.com/C1HRqNyKf0zQqUQuzeBiSY5AfdQ=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-14318428-1572124373-3271.jpeg.jpg"></img></td>
+            </tr>
+            <tr>
+                <td>Pleine Lune</td>
+                <td>2018</td>
+                <td><img src="https://img.discogs.com/Ow1JgvkHTBtK9k9BvTyu9VKOtJY=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-12987000-1545944889-6386.jpeg.jpg"></img></td>
+            </tr>
+        </table>
+
+    )
+}
+
+export default Songs
