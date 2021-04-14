a9cae063c01196b18fafc37e2a4401fca1db45b4
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-14 11:45:59 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { About } from './pages'
+import { About, Home, Technology } from './pages'
+import { Switch, Route } from 'react-router-dom';
+
 
 const App = () => {
-return(
-    < About />
-    )
+    return(
+        <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/technology" component={Technology} />
+        </Switch>
+        )
 }
 
 export default App
---
+++ src/pages/Technology/index.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+const Technology = () => {
+
+    return(
+        <h1>This is the Technology Page</h1>
+
+    )
+}
+
+
+
+
+
+export default Technology
