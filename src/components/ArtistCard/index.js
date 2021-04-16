bb06ec2cac990110e2828f84e0388a753a2eba37
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-16 14:10:40 +0100

    add artist data to Artist component

--- src/components/ArtistCard/index.js
+++ src/components/ArtistCard/index.js
@@ -4,9 +4,7 @@ import {Like, Albums} from '..'
 
 
 const ArtistCard = (props) => {
-    const albums = [['Come and Take it all', 'Crossing the threshold', 'The free-the will'], 
-                    ['Birdy','Young Heart','Beautiful Lies'], ['PlEINE LUNE', 'PLEINE LUNE 2'], ['I forget where we were', 'Noonday Dream'],['test','test']
-                ]
+  
 
     const[moreInfo, setmoreInfo] = useState(false)
 
@@ -16,15 +14,13 @@ const ArtistCard = (props) => {
 
     if (moreInfo){
    
-
-
     return(
         <div className="artistCard">
             <div className="artist">
         <h2 onClick={showmore}>{props.name}</h2>
         <img src={props.artistImg}/>
         <Like/>
-        <Albums albums ={albums[(props.id)-1]}/>
+        <Albums albums ={props.albums}/>
         </div>
         </div>
     )
--- src/pages/Artists/index.js
+++ src/pages/Artists/index.js
@@ -6,10 +6,10 @@ import './style.css'
 
 const Artists = () => {
 
-    const artists = [{ id: 1, name: "Black Coffee", artistImg:"https://www.ravejungle.com/wp-content/uploads/2018/11/black-coffee.jpg"}, 
-    {id:2, name: "Birdy", artistImg:"https://upload.wikimedia.org/wikipedia/en/thumb/4/45/Birdy_%28Birdy_album%29.png/220px-Birdy_%28Birdy_album%29.png"}, 
-    {id: 3, name: "Sofiane Pamart", artistImg: "https://herbertmag.com/wp-content/uploads/2020/11/Sofiane-final-scaled.jpg"},
-     {id:4, name: "Ben Howard", artistImg:"https://www.nme.com/wp-content/uploads/2021/01/BEN-HOWARD-2021.jpg"},
+    const artists = [{ id: 1, name: "Black Coffee", artistImg:"https://www.ravejungle.com/wp-content/uploads/2018/11/black-coffee.jpg", Albums: ['Come and Take it all', 'Crossing the threshold', 'The free-the will']}, 
+    {id:2, name: "Birdy", artistImg:"https://upload.wikimedia.org/wikipedia/en/thumb/4/45/Birdy_%28Birdy_album%29.png/220px-Birdy_%28Birdy_album%29.png", Albums: ['Birdy','Young Heart','Beautiful Lies']}, 
+    {id: 3, name: "Sofiane Pamart", artistImg: "https://herbertmag.com/wp-content/uploads/2020/11/Sofiane-final-scaled.jpg", Albums: ['PlEINE LUNE', 'PLEINE LUNE 2']},
+     {id:4, name: "Ben Howard", artistImg:"https://www.nme.com/wp-content/uploads/2021/01/BEN-HOWARD-2021.jpg", Albums: ['I forget where we were', 'Noonday Dream']},
     {id:5, name: 'test'}]
 
  return (
@@ -24,7 +24,8 @@ const Artists = () => {
 
         <div className="artists">
             {artists.map(artist => 
-                <ArtistCard key={ artist.id} id = {artist.id} name={artist.name} artistImg={artist.artistImg}/>)}
+                <ArtistCard key={ artist.id} id = {artist.id} name={artist.name} artistImg={artist.artistImg} albums ={artist.Albums}/>)}
+                {/* I could wite <ArtistCard {...artist}/>*/}
         </div>
 
      
